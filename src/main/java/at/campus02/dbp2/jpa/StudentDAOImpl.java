7d4eb507538714f3a661fb3a5fe154035ccb3d2e
kbuhlert <kbuhlert@gmail.com>
2020-10-22 23:51:50 +0200

    Implementing Tests

--- src/main/java/at/campus02/dbp2/jpa/StudentDAOImpl.java
+++ src/main/java/at/campus02/dbp2/jpa/StudentDAOImpl.java
@@ -2,6 +2,12 @@ package at.campus02.dbp2.jpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class StudentDAOImpl implements StudentDAO {
@@ -14,9 +20,13 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public boolean create(Student student) {
-        if(student == null){
+        if(student == null)
             return false;
-        }
+
+        if ((student.getId()) != null){
+                return false;
+            }
+
         manager.getTransaction().begin();
         manager.persist(student);   //persist = in Datenbank eintragen
         manager.getTransaction().commit();
@@ -25,41 +35,79 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public Student update(Student student) {
-        return null;
+        //abfrage ob Student = null ist (Objekt/Parameter nicht existiert) oder ob keine passende ID gefunden werden kann
+        if(student == null || find(student.getId()) == null){
+            return null;
+        }
+        manager.getTransaction().begin();
+        Student updated = manager.merge(student);   //Objekt wird aus DB ausgelesen und ist somit detached. Mit merge wird Objekt wieder mit Datenbank verbunden, ansonsten wäre es keine gemanagte Entity
+        manager.getTransaction().commit();
+        return updated;
     }
 
     @Override
     public void delete(Student student) {
 
+        if(student == null || find(student.getId()) == null){
+            return;
+        }
+    manager.getTransaction().begin();
+    Student removed = manager.merge(student);
+    manager.remove(removed);
+    manager.getTransaction().commit();
     }
 
     @Override
     public Student find(Integer id) {
-        return null;
+        if(id==null)
+            return null;
+        return manager.find(Student.class, id);
     }
 
     @Override
     public List<Student> findAll() {
-        return null;
+        String querystring = "SELECT s FROM Student s";
+        TypedQuery<Student> query = manager.createQuery(querystring, Student.class);
+        return query.getResultList();
     }
 
     @Override
     public List<Student> findAllByLastname(String lastname) {
-        return null;
+        if (lastname == null)
+            return findAll();
+        String querystring = "SELECT s FROM Student s WHERE upper(s.lastName) = upper(:lastname)";   //mach mit upper aus allen Studenten Studenten mit Großbuchstaben ->Vergleichbarkeit der Einträge
+        TypedQuery<Student> query = manager.createQuery(querystring, Student.class);
+        query.setParameter("lastname", lastname);
+        return query.getResultList();
     }
 
     @Override
     public List<Student> findAllBornBefore(int year) {
-        return null;
+        LocalDate firstDayOfYear = LocalDate.of(year, Month.JANUARY, 1);
+        String querystring = "SELECT s FROM Student s WHERE s.birthday < :firstDayOfYear";
+        TypedQuery<Student> query = manager.createQuery(querystring, Student.class);
+        query.setParameter("firstDayOfYear", firstDayOfYear);
+
+        return query.getResultList();
     }
 
     @Override
     public List<Student> findAllByGender(Gender gender) {
-        return null;
+        if (gender == null)
+            return Collections.EMPTY_LIST;
+
+       // String querystring = "SELECT s FROM Student s WHERE s.gender = :gender";
+        // TypedQuery<Student> query = manager.createQuery(querystring, Student.class);
+
+        //Alternativer Query mit dem NamedQuery aus der Klasse Student:
+        TypedQuery<Student> query = manager.createNamedQuery("Student.findAllByGender", Student.class);
+        query.setParameter("gender", gender);
+        return query.getResultList();
     }
 
     @Override
     public void close() {
-
+        if(manager!= null && manager.isOpen())
+            manager.close();
     }
 }
--- src/test/java/StudentDAOSpec.java
+++ src/test/java/StudentDAOSpec.java
@@ -3,13 +3,20 @@ import at.campus02.dbp2.jpa.Student;
 import at.campus02.dbp2.jpa.StudentDAO;
 import at.campus02.dbp2.jpa.StudentDAOImpl;
 import org.hamcrest.CoreMatchers;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.text.DateFormatter;
 import javax.swing.text.html.parser.Entity;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;  //der Teil, wird bei Verwendung dieser Klasse immmer automatisch zigefügt
 import static org.junit.Assert.assertThat;
@@ -18,7 +25,43 @@ public class StudentDAOSpec {
     //Klasse für Tests, Tests sind Methoden der Klasse die ausgeführt werden
     //Strukturierung der tests in 3 Blöckke given(Vorraussetzungen die gebraucht werden um Test durchzuführen,
     // when (was getestet werden soll), then (überprüfen ob funktioniert)
+    private EntityManager manager;
+    private EntityManagerFactory factory;
+    private StudentDAO dao;
 
+
+    // <editor-fold desc="Hilfsfunktionen">
+    private Student prepareStudent(
+            String firstname,
+            String lastname,
+            Gender gender,
+            String birthdayString
+    ){
+        Student student = new Student();
+        student.setGender(gender);
+        student.setFirstName(firstname);
+        student.setLastName(lastname);
+        if(birthdayString != null){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            student.setBirthday(LocalDate.parse(birthdayString,formatter));
+        }
+        return student;
+    }
+    // <editor-fold desc=Hilfsfunktionen>
+
+    @Before
+    public void setUp(){
+        factory = Persistence.createEntityManagerFactory("nameOfJpaPersistenceUnit");
+        manager = factory.createEntityManager();
+        dao = new StudentDAOImpl(factory);
+    }
+
+    //<editor-fold desc="Queries>
+    private void create(Student student){
+        manager.getTransaction().begin();
+        manager.persist(student);
+        manager.getTransaction().commit();
+    }
 
    //wenn Test nicht berücksichtigt werden soll, dann kann mit der @Ignore Annotation der Test ausgelassen werden.
     @Test
@@ -37,7 +80,7 @@ public class StudentDAOSpec {
 
     @Test
     public void createNullAsStudentReturnsFalse(){
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("nameOfJpaPersistenceUnit");
+
 
         //given
         StudentDAO dao = new StudentDAOImpl(factory);
@@ -50,14 +93,13 @@ public class StudentDAOSpec {
     @Test
     public void createPersitsStudentInDatabaseAndReturnsTrue(){
         //Testet ob Student tatsäschlich angelegt wird und somit auch aus Datenbank ausgelesen werden kann
-        //given
-       EntityManagerFactory factory = Persistence.createEntityManagerFactory("nameOfJpaPersistenceUnit");
 
-        Student student = new Student();
+        //given
+        Student student = prepareStudent("firstname", "lastname", Gender.FEMALE, null);
+
         student.setLastName("Lastname");
         student.setFirstName("Firstname");
         student.setGender(Gender.FEMALE);
-        StudentDAO dao = new StudentDAOImpl(factory);
 
         //when
         boolean result = dao.create(student);
@@ -69,8 +111,300 @@ public class StudentDAOSpec {
         EntityManager manager = factory.createEntityManager();
         Student fromDB = manager.find(Student.class, student.getId());
         assertThat(fromDB, is(student));
+    }
+
+    @Test
+    public void createAlreadyExistingStudentReturnsFalse(){
+        //wir testen, ob wenn ein bereits existierender Studen in DB eingelesen wird, ob dann auch tatsächlich falsch ausgegben wird
+        //given
+        Student student = prepareStudent("firstname", "lastname", Gender.FEMALE, "22.09.1978");
+        create(student);
+
+        //when
+        boolean result = dao.create(student);
+
+        //then
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void findStudentReturnsEntityFromDatabase(){
+        //given
+        Student student = prepareStudent("firstname", "lastname", Gender.FEMALE, "22.09.1978");
+        create(student);
+
+        //when
+        //der Student, den wir vom DAO bekommen:
+        Student result = dao.find(student.getId());
+        //...un der Student, den wir im Test aus der DB lesen...
+        Student fromDB = manager.find(Student.class, student.getId());
+
+        //then
+
+
+        //...sollen die gleichen sein
+        assertThat(result, is(fromDB));
+    }
+
+    @Test
+    public void findStudentWithNullAsIdReturnsNull(){
+        //Bei Angabe einer nicht existierenden ID (oder null) soll null zurückgegeben werden.
+        //expect
+        assertThat(dao.find(null), is(nullValue()));
+    }
+
+    @Test
+    public void findStudentWithNotExistingIDReturnsNull(){
+        //Bei Angabe einer nicht existierenden ID (oder null) soll null zurückgegeben werden.
+        //expect
+        assertThat(dao.find(4711), is(nullValue()));
+    }
+
+    @Test
+    public void updateStudentChangesValuesInDatabase(){
+        //Nach erfolgreichem Aufruf soll die Entity in der Datenbank die geänderten Werte übernommen haben und das geänderte Objekt zurückgegeben werden.
+
+        //given
+        Student student = prepareStudent("firstname", "lastname", Gender.FEMALE, "22.09.1978");
+        create(student);
+
+        //vorsichtahalber Cache ausleeren, weil wir denselben EntityManager für persist und dann find verwenden...
+        manager.clear();
+
+        //when
+        //Namensänderung
+        student.setLastName("Married-now");
+        //geänderter Student wird
+        Student result = dao.update(student);
+        //...un der Student, den wir im Test aus der DB lesen...
+        Student fromDB = manager.find(Student.class, student.getId());
+
+        //then
+        //...sollen die gleichen sein (Rückgabe wert (result) und die Entity aus DB (fromDB))
+        assertThat(result.getLastName(), is("Married-now"));
+        assertThat(fromDB.getLastName(), is("Married-now"));
+        //test ob beide Objekte gleich sind (wäre nicht notwendig
+        assertThat(result, is(fromDB));
+    }
+
+    @Test
+    public void updateNullAsStudentReturnsNull(){
+        //Bei Angabe eines nicht existierenden Studenten soll null zurückgegeben werden.
+        //expect
+        assertThat(dao.update(null), is(nullValue()));
+    }
+
+    @Test
+    public void updateNotExistingStudentReturnsNull(){
+        //Bei Angabe eines nicht existierenden Studenten soll null zurückgegeben werden.
+        //expect
+        //given
+        Student student = prepareStudent("firstname", "lastname", Gender.FEMALE, "22.09.1978");
+
+        //when
+        Student result = dao.update(student);
+
+        //then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    public void deleteStudentRemovesEntityFromDatabase(){
+        //Bei Angabe eines nicht existierenden Studenten soll null zurückgegeben werden.
+        //expect
+        //given
+        Student student = prepareStudent("firstname", "lastname", Gender.FEMALE, "22.09.1978");
+        create(student);
+        manager.clear();
+
+        //when
+        int id = student.getId();
+        dao.delete(student);
+
+        //then
+        assertThat(dao.find(id), is(nullValue()));
+        assertThat(manager.find(Student.class, id), is(nullValue()));
+    }
+
+    @Test
+    public void deleteNullOrNotExistingStudentDoesNotThrowException(){
+        //expect no Exception
+        dao.delete(null);
+        dao.delete(prepareStudent("firstname", "lastname", Gender.FEMALE, "22.09.1978"));
+    }
+
+    @Test
+    public void findAllReturnsAllEntitiesFromDatabase(){
+        //given
+        Student student1 = prepareStudent("firstname1", "lastname1", Gender.FEMALE, "22.09.1978");
+        Student student2 = prepareStudent("firstname2", "lastname2", Gender.FEMALE, "22.09.1978");
+        Student student3 = prepareStudent("firstname3", "lastname3", Gender.MALE, "22.09.1978");
+
+        create(student1);
+        create(student2);
+        create(student3);
+
+        manager.clear();    //wegen caching vom manager müssen wir sicher sein, dass er wert aus DB holt und nicht aus Cache, deshalb leeren
+
+        //when
+        List<Student> result = dao.findAll();   //nullpointer
+
+        //then
+        assertThat(result.size(), is(3));
+        assertThat(result, hasItems(student1, student2, student3));
+    }
+
+
+    @Test
+    public void findByLastnameReturnsMatchingStudents() {
+        //given
+        String lastNameToFind = "lastname1";
+
+        Student student1 = prepareStudent("firstname1", lastNameToFind, Gender.FEMALE, "22.09.1978");
+        Student student2 = prepareStudent("firstname2", "lastname2", Gender.FEMALE, "22.09.1978");
+        Student student3 = prepareStudent("firstname3", lastNameToFind, Gender.MALE, "22.09.1978");
+
+        create(student1);
+        create(student2);
+        create(student3);
+
+        //when
+        List<Student> result = dao.findAllByLastname(lastNameToFind);
+
+        //then
+        assertThat(result.size(), is(2));
+        assertThat(result, hasItems(student1, student3));
+    }
+
+    @Test
+    public void findByLastNameWithNullParameterReturnsAllEntities() {
+        //hierfür machen wir das gleich wie findByLastname, prüfen aber ob alle Studenten in zurückgegebener Liste sind, wenn lastname=null
+        //given
+        String lastNameToFind = "lastname1";
+
+        Student student1 = prepareStudent("firstname1", lastNameToFind, Gender.FEMALE, "22.09.1978");
+        Student student2 = prepareStudent("firstname2", "lastname2", Gender.FEMALE, "22.09.1978");
+        Student student3 = prepareStudent("firstname3", "lastname3", Gender.MALE, "22.09.1978");
+
+        create(student1);
+        create(student2);
+        create(student3);
+
+        //when
+        List<Student> result = dao.findAllByLastname(null);
+
+        //then
+        assertThat(result.size(), is(3));
+        assertThat(result, hasItems(student1, student2, student3));
+    }
+
+    @Test
+    public void findByLastnameReturnsMatchingStudentsCaseInsensitive() {
+        //given
+        String lastNameLoweCase = "lastname1";
+        String lastNameUpperCase = "Lastname1";
+
+        Student student1 = prepareStudent("firstname1", lastNameLoweCase, Gender.FEMALE, "22.09.1978");
+        Student student2 = prepareStudent("firstname2", "lastname2", Gender.FEMALE, "22.09.1978");
+        Student student3 = prepareStudent("firstname3", lastNameUpperCase, Gender.MALE, "22.09.1978");
+
+        create(student1);
+        create(student2);
+        create(student3);
+
+        //when
+        List<Student> result = dao.findAllByLastname(lastNameUpperCase);
+
+        //then
+        assertThat(result.size(), is(2));
+        assertThat(result, hasItems(student1, student3));
+    }
+
+    @Test
+    public void findAllByGenderReturnsMatchingEntities() {
+        //hierfür machen wir das gleich wie findByLastname, prüfen aber ob alle Studenten in zurückgegebener Liste sind, wenn lastname=null
+        //given
+        Gender genderEnum = Gender.FEMALE;
+
+        Student student1 = prepareStudent("firstname1", "lastname1", genderEnum, "15.09.1937");
+        Student student2 = prepareStudent("firstname2", "lastname2", genderEnum, "22.09.1978");
+        Student student3 = prepareStudent("firstname3", "lastname3", Gender.MALE, "22.09.1978");
+
+        create(student1);
+        create(student2);
+        create(student3);
+
+        //when
+        List<Student> result = dao.findAllByGender(genderEnum);
+
+        //then
+        assertThat(result.size(), is(2));
+        assertThat(result, hasItems(student1, student2));
+    }
+
+    @Test
+    public void findAllByGenderWithNullReturnsEmptyList() {
+        //hierfür machen wir das gleich wie findByLastname, prüfen aber ob alle Studenten in zurückgegebener Liste sind, wenn lastname=null
+        //given
+        Gender genderEnum = Gender.FEMALE;
+
+        Student student1 = prepareStudent("firstname1", "lastname1", genderEnum, "15.09.1937");
+        Student student2 = prepareStudent("firstname2", "lastname2", genderEnum, "22.09.1978");
+        Student student3 = prepareStudent("firstname3", "lastname3", Gender.MALE, "22.09.1978");
+
+        create(student1);
+        create(student2);
+        create(student3);
+
+        //when
+        List<Student> result = dao.findAllByGender(null);
+
+        //then
+        assertThat(result.isEmpty(), is(true));
+    }
+
+    @Test
+    public void findAllBornBeforeReturnsMatchingEntities() {
+        //given
+        String birthdayString = "22.09.1978";
+
+        Student student1 = prepareStudent("firstname1", "lastname1", Gender.FEMALE, "15.09.1937");
+        Student student2 = prepareStudent("firstname2", "lastname2", Gender.FEMALE, "22.09.1978");
+        Student student3 = prepareStudent("firstname3", "lastname3", Gender.MALE, "22.09.1980");
+        Student student4 = prepareStudent("firstname4", "lastname4", Gender.MALE, null);
+
+        create(student1);
+        create(student2);
+        create(student3);
+        create(student4);
+
+        //when
+        List<Student> result = dao.findAllBornBefore(1977);
+
+        //then
+        assertThat(dao.findAllBornBefore(1980), hasItems(student1, student2));
+        assertThat(dao.findAllBornBefore(1980).size(), is(2));
+        assertThat(dao.findAllBornBefore(1981), hasItems(student1, student2, student3));
+        assertThat(dao.findAllBornBefore(1981).size(), is(3));
+
+        assertThat(dao.findAllBornBefore(1920).isEmpty(), is(true));
 
     }
+
+    @After
+    public void tearDown(){
+        //Die close-Methode soll am Ende jedes Tests aufgerufen werden
+        // und sicherstellen, dass die verwendeten Ressourcen (EntityManager) aufgeräumt werden.
+        //nach jedem Test wird aufgeräumt
+        dao.close();
+        if(manager.isOpen()){
+            manager.close();
+        }
+        if(factory.isOpen()){
+            factory.close();
+        }
+    }
+
 
 
 }
